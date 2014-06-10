4e625dffe037372316de3af50bca6f597678067d
beckerdad <beckermom@gmail.com>
2014-06-10 15:31:07 +0200

    Start ground resonance program

---
+++ groundResonanceModel/things/checkBlue/checkBlue/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using Microsoft.SPOT;
+using Microsoft.SPOT.Hardware;
+using SecretLabs.NETMF.Hardware;
+using SecretLabs.NETMF.Hardware.Netduino;
+using Marenco.Comms;
+
+namespace checkBlue
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // write your code here
+
+            BlueSerial ser = new BlueSerial();
+
+            int j = 0;
+
+            while (true)
+            {
+                Thread.Sleep(500);
+                j++;
+                ser.Print(j);
+
+            }
+        }
+
+    }
+}
