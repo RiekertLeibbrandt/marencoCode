490971e398271701c30db84a0b08ddc8a323c1bd
beckerdad <beckermom@gmail.com>
2014-07-08 16:26:22 +0200

    Got sick and tired of Bluetooth. Move to UDP.

--- groundResonanceModel/marencoTune/marencoTune/globalVars.cs
+++ groundResonanceModel/marencoTune/marencoTune/globalVars.cs
@@ -12,12 +12,15 @@ namespace marencoTune
 
     public static class GlobalVariables
     {
-        public static byte[] throttleSetting = new byte[1] { 0 };
-        public static byte throttleSettingOld = 0x12;
+        public static int rampUpPeriod = 100;
+
+        public static byte[] throttleSetting = new byte[10] { 0, 22, 0,0,0,0,0,0,0,0};
         public static int x = 0;
         public static int y = 0;
         public static int z = 0;
         public static UInt32 halTime = 0;
-
+        public static bool writeNow = false;
+        public static Int16 zKeep = 0;
+        public static byte throttleSettingOld = 0x3C;
     }
 }
